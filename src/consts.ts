4fe13c966edf91822c1ccc40b28caa14e0dd9e69
autroshot <autroshot@gmail.com>
2023-08-31 17:05:37 +0900

    최상단 상수값 변경

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dev Archive';
+export const SITE_DESCRIPTION =
+  '개발과 관련된 좋은 글을 번역하거나 정리하는 곳입니다.';
