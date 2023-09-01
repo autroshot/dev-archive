5fe703fb0e36dadf52d50cf1e119f192c880adf0
autroshot <autroshot@gmail.com>
2023-09-02 02:27:18 +0900

    해결: 잘못된 RSS 설정

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,18 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
-export async function get(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+export async function GET(context) {
+  const posts = await getCollection('blog');
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.publishedDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
 }
