1bd7979f4a7fdd9f050e15a1f64a9e34c3e2d81b
autroshot <autroshot@gmail.com>
2023-09-01 14:56:11 +0900

    astro-expressive-code 패키지 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import astroExpressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap()],
+  integrations: [sitemap(), astroExpressiveCode()],
 });
