282c7b5a500a06e9c93bfd76f5e45eaa813f1979
autroshot <autroshot@gmail.com>
2023-09-01 15:44:39 +0900

    코드 블록의 복사 버튼의 텍스트 번역

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
-import astroExpressiveCode from 'astro-expressive-code';
+import astroExpressiveCode, { pluginFramesTexts } from 'astro-expressive-code';
+
+pluginFramesTexts.addLocale('ko', {
+  copyButtonCopied: '복사됨!',
+  copyButtonTooltip: '클립보드에 복사하기',
+  terminalWindowFallbackTitle: '터미널 윈도우',
+});
+
+/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+const astroExpressiveCodeOptions = {
+  styleOverrides: {
+    codeFontSize: '16px',
+    codeLineHeight: '1.5em',
+  },
+  defaultLocale: 'ko',
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap(), astroExpressiveCode()],
+  integrations: [sitemap(), astroExpressiveCode(astroExpressiveCodeOptions)],
 });
