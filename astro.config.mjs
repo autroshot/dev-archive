89e24dd645f9c3d60f5381cf1f898ebf1b0049f6
autroshot <autroshot@gmail.com>
2023-09-13 03:26:15 +0900

    리팩터: 프리티어 설정 변경에 따른 포맷 적용

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import astroExpressiveCode, { pluginFramesTexts } from "astro-expressive-code";
-import { defineConfig } from "astro/config";
-import rehypeExternalLinks from "rehype-external-links";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import astroExpressiveCode, { pluginFramesTexts } from 'astro-expressive-code';
+import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
 
-pluginFramesTexts.addLocale("ko", {
-  copyButtonCopied: "복사됨!",
-  copyButtonTooltip: "클립보드에 복사하기",
-  terminalWindowFallbackTitle: "터미널 윈도우",
+pluginFramesTexts.addLocale('ko', {
+  copyButtonCopied: '복사됨!',
+  copyButtonTooltip: '클립보드에 복사하기',
+  terminalWindowFallbackTitle: '터미널 윈도우',
 });
 
 /** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
@@ -16,24 +16,34 @@ const astroExpressiveCodeOptions = {
   frames: {
     showCopyToClipboardButton: true,
     styleOverrides: {
-      tooltipSuccessBackground: "var(--code-tooltip-success-background)",
+      tooltipSuccessBackground: 'var(--code-tooltip-success-background)',
     },
   },
   styleOverrides: {
-    codeFontSize: "var(--code-font-size)",
-    codeFontFamily: "var(--code-font-family)",
+    codeFontSize: 'var(--code-font-size)',
+    codeFontFamily: 'var(--code-font-family)',
   },
-  defaultLocale: "ko",
+  defaultLocale: 'ko',
 };
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [sitemap(), astroExpressiveCode(astroExpressiveCodeOptions), tailwind(), mdx()],
+  site: 'https://example.com',
+  integrations: [
+    sitemap(),
+    astroExpressiveCode(astroExpressiveCodeOptions),
+    tailwind(),
+    mdx(),
+  ],
   markdown: {
-    rehypePlugins: [[rehypeExternalLinks, {
-      target: "_blank",
-      rel: ["noopener", "noreferrer"],
-    }]],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['noopener', 'noreferrer'],
+        },
+      ],
+    ],
   },
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,74 +1,74 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        "nanum-gothic": ['"Nanum Gothic"', "sans-serif"],
+        'nanum-gothic': ['"Nanum Gothic"', 'sans-serif'],
       },
       typography: (theme) => ({
         DEFAULT: {
           css: {
             a: {
-              textDecoration: "underline",
-              textUnderlinePosition: "under",
-              color: theme("colors.cyan.700"),
+              textDecoration: 'underline',
+              textUnderlinePosition: 'under',
+              color: theme('colors.cyan.700'),
             },
-            "a:hover": { textDecoration: "initial" },
-            "ol > li::marker": {
-              color: theme("colors.gray.700"),
+            'a:hover': { textDecoration: 'initial' },
+            'ol > li::marker': {
+              color: theme('colors.gray.700'),
             },
-            "ul > li::marker": {
-              color: theme("colors.gray.500"),
+            'ul > li::marker': {
+              color: theme('colors.gray.500'),
             },
             blockquote: {
-              fontStyle: "normal",
-              borderLeftColor: theme("colors.gray.400"),
+              fontStyle: 'normal',
+              borderLeftColor: theme('colors.gray.400'),
             },
-            "blockquote p:first-of-type::before": {
-              content: "",
+            'blockquote p:first-of-type::before': {
+              content: '',
             },
-            "blockquote p:last-of-type::after": {
-              content: "",
+            'blockquote p:last-of-type::after': {
+              content: '',
             },
             img: {
-              margin: "0 auto",
-              borderRadius: "8px",
+              margin: '0 auto',
+              borderRadius: '8px',
             },
             code: {
               fontFamily:
                 "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
-              color: "inherit",
-              fontWeight: "inherit",
-              backgroundColor: theme("colors.gray.200"),
-              padding: "2px 5px",
-              borderRadius: "3px",
+              color: 'inherit',
+              fontWeight: 'inherit',
+              backgroundColor: theme('colors.gray.200'),
+              padding: '2px 5px',
+              borderRadius: '3px',
             },
-            "code::before": {
-              content: "",
+            'code::before': {
+              content: '',
             },
-            "code::after": {
-              content: "",
+            'code::after': {
+              content: '',
             },
           },
         },
         sm: {
           css: {
             code: {
-              fontSize: "14px",
+              fontSize: '14px',
             },
           },
         },
         base: {
           css: {
-            fontSize: "17px",
+            fontSize: '17px',
             code: {
-              fontSize: "17px",
+              fontSize: '17px',
             },
           },
         },
       }),
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
